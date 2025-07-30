52b04b04c2bb84375d046ed8fb20acda2eaa4fb5
shashank2796 <31096925+shashank2796@users.noreply.github.com>
2025-07-30 10:09:39 +0530

    Add files via upload
    Chatbot files

---
+++ utils.py
@@ -0,0 +1,53 @@
+from openai import OpenAI 
+import os
+# os.environ["SSL_CERT_FILE"] = certifi.where()
+# os.environ["SSL_CERT_DIR"] = "C:\Users\A0101150\OneDrive - KION Group\Shashank_Projects\17-KTCI_Chatbot\kion_chatbot\lib\site-packages\certifi\cacert.pem"
+# os.environ["http_proxy"]  = "http://proxy.company.com:8080"
+# os.environ["https_proxy"] = "http://proxy.company.com:8080"
+from dotenv import load_dotenv
+import base64
+import streamlit as st
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+def get_answer(messages):
+    system_message = [{"role": "system", "content": "You are a helpful AI chatbot that answers questions asked by the User."}]
+    messages = system_message + messages
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=messages
+    )
+    return response.choices[0].message.content
+
+def speech_to_text(audio_data):
+    with open(audio_data, "rb") as audio_file:
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            response_format="text",
+            file=audio_file
+        )
+    return transcript
+
+def text_to_speech(input_text):
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="nova",
+        input=input_text
+    )
+    webm_file_path = "temp_audio_play.mp3"
+    with open(webm_file_path, "wb") as f:
+        response.stream_to_file(webm_file_path)
+    return webm_file_path
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+    b64 = base64.b64encode(data).decode("utf-8")
+    md = f"""
+    <audio autoplay=>
+    <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+    </audio>"""
+    
+    st.markdown(md, unsafe_allow_html=True)
